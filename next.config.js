b1567dca776406fa737e1dadd7651b11d6e4465e
NEMILIA <nemilia.ici@gmail.com>
2026-08-06 18:58:32 +0000

    Fix: Mover configuración de ESLint de next.config.js a .eslintrc.json

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
